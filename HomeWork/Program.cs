f7cba5ed45b2da8edc8dca25457af1967343163f
Maxim Pokrovsky <maxim.pokrovsky@outlook.com>
2022-03-02 20:59:24 +0300

    Подготовка рабочей среды к выполнению ДЗ.

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace HomeWork
 {
+    //author: Maxim Pokrovsky
     internal class Program
     {
         static void Main(string[] args)
