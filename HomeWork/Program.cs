48dfaf51c19ee8684e53ecd72ccbfdcee5f5e3ee
Maxim Pokrovsky <maxim.pokrovsky@outlook.com>
2022-03-09 18:41:39 +0300

    Написал первую программу, в которой метод возвращает наименьшее из трех чисел.

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -9,8 +9,47 @@ namespace HomeWork
     {
         static void Main(string[] args)
         {
-
+            Task1();
         }
+
+        #region Task 01
+
+        /*
+                Написать метод, возвращающий минимальное из трёх чисел. 
+         */
+
+        static void Task1()
+        {
+            ConsoleHelper.StartSettings("Минимальное из трёх чисел");
+            Console.Write("Введите первое число: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Введите третье число: ");
+            int c = int.Parse(Console.ReadLine());
+            int minimum = GetMinimum(a, b, c);
+            Console.WriteLine($"Из чисел {a}, {b} и {c} минимальное: {minimum}");
+            ConsoleHelper.Pause();
+        }
+
+        static int GetMinimum(int a, int b, int c)
+        {
+            int min = a;
+
+            if (b < min)
+            {
+                min = b;
+            }
+
+            if (c < min)
+            {
+                min = c;
+            }
+
+            return min;
+        }
+
+        #endregion
 
     }
 
