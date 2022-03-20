227b8a59e59447153b12ae911602648e184afae4
Maxim Pokrovsky <maxim.pokrovsky@outlook.com>
2022-03-20 18:10:27 +0300

    Подготовка прогрраммы к третьему ДЗ.

---
+++ MP.Utils/MathHelper.cs
@@ -0,0 +1,24 @@
+﻿namespace MP.Utils
+{
+    public class MathHelper
+    {
+        public static int GetNumberOfDigits(long number)
+        {
+            int count = 0;
+
+            do
+            {
+                count++;
+                number /= 10;
+            }
+            while (number != 0);
+
+            return count;
+        }
+
+        static bool CheckOddNumber(int number)
+        {
+            return number % 2 != 0;
+        }
+    }
+}
