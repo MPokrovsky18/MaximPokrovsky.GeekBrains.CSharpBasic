4ab1292db552a6ef42c7d5304f41e2a43e54f0b6
Maxim Pokrovsky <maxim.pokrovsky@outlook.com>
2022-03-27 19:27:25 +0300

    Перенес классы для работы с массивами в свою библиотеку классов MP.Utils. Рефакторинг.

---
+++ MP.Utils/MyArray.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+
+namespace MP.Utils
+{
+    public class MyArray
+    {
+        Random random = new Random();
+        private int[] _array;
+
+        public int Max
+        {
+            get
+            {
+                return _array.Max();
+            }
+        }
+        public int Sum
+        {
+            get
+            {
+                return _array.Sum();
+            }
+        }
+        public int MaxCount
+        {
+            get
+            {
+                int count = 0;
+                int max = Max;
+
+                foreach (int el in _array)
+                {
+                    if (el == max)
+                    {
+                        count++;
+                    }
+                }
+
+                return count;
+            }
+        }
+
+        public int this[int i]
+        {
+            get
+            {
+                return _array[i];
+            }
+            set
+            {
+                _array[i] = value;
+            }
+        }
+
+        public MyArray(int length)
+        {
+            _array = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                _array[i] = random.Next(1, 101);
+            }
+        }
+
+        public MyArray(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                string[] ss = File.ReadAllLines(filename);
+                _array = new int[ss.Length];
+
+                for (int i = 0; i < ss.Length; i++)
+                {
+                    _array[i] = int.Parse(ss[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error load file");
+            }
+        }
+
+        public MyArray(int length, int startValue, int step)
+        {
+            _array = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                _array[i] = startValue;
+                startValue += step;
+            }
+        }
+
+        public int[] Inverse()
+        {
+            int[] inverseArray = new int[_array.Length];
+
+            for (int i = 0; i < inverseArray.Length; i++)
+            {
+                inverseArray[i] = -_array[i];
+            }
+
+            return inverseArray;
+        }
+
+        public void Multi(int multiplier)
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                _array[i] *= multiplier;
+            }
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+
+            foreach (int el in _array)
+            {
+                s += ("{0,4}", el);
+            }
+
+            return s;
+        }
+    }
+}
---
+++ MP.Utils/StaticClass.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+
+
+namespace MP.Utils
+{
+    public class StaticClass
+    {
+        public static int[] CreateAndFillArray(uint length, int min = 0, int max = 100)
+        {
+            Random random = new Random();
+            int[] array = new int[length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(min, max + 1);
+            }
+
+            return array;
+        }
+
+        public static void PrintArray(int[] array)
+        {
+            if (array != null)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write("|" + array[i]);
+                }
+
+                Console.WriteLine("|");
+            }
+        }
+
+        public static void PrintArray(int[] array, string message)
+        {
+            if (array != null)
+            {
+                Console.WriteLine(message);
+                PrintArray(array);
+            }
+        }
+
+        public static int GetCountPair(int[] array, int divider)
+        {
+            int count = 0;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] % divider == 0 ^ array[i + 1] % divider == 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public static int[] GetArrayFromFile(string path)
+        {
+            if (File.Exists(path) == false || File.ReadAllLines(path).Length == 0)
+            {
+                return null;
+            }
+
+            StreamReader sr = new StreamReader(path);
+
+            try
+            {
+                int lenght = int.Parse(sr.ReadLine());
+                int[] array = new int[lenght];
+
+                for (int i = 0; i < lenght; i++)
+                {
+                    array[i] = int.Parse(sr.ReadLine());
+                }
+
+                sr.Close();
+                return array;
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+
+            return null;
+        }
+
+        public static void SetArrayToFile(int[] array, string path)
+        {
+            if (array == null)
+            {
+                Console.WriteLine("Массив равен null");
+                return;
+            }
+
+            if (File.Exists(path))
+            {
+                if (File.ReadAllLines(path).Length > 0)
+                {
+                    Console.Write("Файл не пуст. Перезаписать? (y/n): ");
+
+                    if (Console.ReadLine().ToLower() != "y")
+                    {
+                        return;
+                    }
+                }
+            }
+
+            File.Create(path).Close();
+            StreamWriter sw = new StreamWriter(path);
+            sw.WriteLine(array.Length);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sw.WriteLine(array[i]);
+            }
+
+            sw.Close();
+        }
+    }
+}
