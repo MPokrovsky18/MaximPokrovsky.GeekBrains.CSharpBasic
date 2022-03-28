132201d95789623b980ca3212846a0515006d37e
Maxim Pokrovsky <maxim.pokrovsky@outlook.com>
2022-03-28 23:16:10 +0300

    Подготовил программу к следующему ДЗ.

---
+++ MP.Utils/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace MP.Utils
+{
+    public struct Account
+    {
+        private string _login;
+        private string _password;
+
+        public string Login
+        {
+            get
+            {
+                return _login;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Логин не может быть null или пустым");
+                }
+
+                value.Replace(" ", "");
+                _login = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Пароль не может быть null или пустым");
+                }
+
+                value.Replace(" ", "");
+                _password = value;
+            }
+        }
+    }
+}
