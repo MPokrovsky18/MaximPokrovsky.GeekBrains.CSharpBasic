260de95d9385108b444bb512ce594abe33e4cb59
Maxim Pokrovsky <maxim.pokrovsky@outlook.com>
2022-04-02 22:31:25 +0300

    Реализовал класс Message по работе с сообщениями.

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Text.RegularExpressions;
 using MP.Utils;
 
@@ -13,7 +14,7 @@ namespace HomeWork
             //ConsoleHelper.StartSettings("Начало программы");
             //ShowMenu();
             //Console.Clear();
-            Task1("Проверка логина");
+            Task2("Класс Message");
             Console.WriteLine("Программа завершена.");
             ConsoleHelper.Pause();
         }
@@ -23,7 +24,7 @@ namespace HomeWork
         static void ShowMenu()
         {
             string nameTask1 = "Проверка логина";
-            string nameTask2 = "";
+            string nameTask2 = "Класс Message";
             string nameTask3 = "";
             string nameTask4 = "";
 
@@ -42,6 +43,7 @@ namespace HomeWork
                         Task1(nameTask1);
                         break;
                     case "2":
+                        Task2(nameTask2);
                         break;
                     case "3":
                         break;
@@ -147,5 +149,92 @@ namespace HomeWork
 
         #endregion
 
+        #region Task 02
+
+        /*
+         
+                 Разработать статический класс Message, содержащий следующие статические методы для обработки текста:
+                    а) Вывести только те слова сообщения, которые содержат не более n букв.
+                    б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+                    в) Найти самое длинное слово сообщения.
+                    г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+         
+         */
+
+        static void Task2(string taskName)
+        {
+            string s = @"Сосны обступали тропу плотно, и, 
+хотя истыканное их верхушками небо светилось голубым, в лесу было сумрачно. 
+По тропинке вперёд бежали муравьи, большие, красные, по своим каким-то муравьиным делам.
+– Смотри, пап, им с нами по пути! – сказала Таня. – Наверное, они тоже на пляж собрались!
+Папа улыбнулся ей сверху.";
+
+            Console.WriteLine("Получен текст:");
+            Console.WriteLine(s);
+            Console.WriteLine("----------------------------------------------------");
+            s = Message.DeleteWords(s, 'е');
+            Console.WriteLine("В тексте удалили слова, которые заканчиваются на букву Е.");
+            Console.WriteLine(s);
+            Console.WriteLine("----------------------------------------------------");
+            Console.Write("Самое длинное слово в тексте: ");
+            Console.WriteLine(Message.GetLongestWord(s));
+            Console.WriteLine("Самые длинные слова в тексте:");
+            Console.WriteLine(Message.GetLongestWordsList(s));
+
+        }
+
+        #endregion
+    }
+
+    public static class Message
+    {
+        private static string[] _separators = { ",", ".", "!", "?", ";", ":", " ", "-" };
+
+        public static void PrintWords(string message, uint maxLength)
+        {
+            string[] words = message.Split(_separators, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length <= maxLength)
+                {
+                    Console.WriteLine(words[i]);
+                }
+            }
+        }
+
+        public static string DeleteWords(string message, char endSymbol)
+        {
+            return new Regex($@"\b\w*{endSymbol}\b", RegexOptions.IgnoreCase).Replace(message, "");
+        }
+
+        public static string GetLongestWord(string message)
+        {
+            string[] words = message.Split(_separators, StringSplitOptions.RemoveEmptyEntries);
+            int indexMaxWord = 0;
+
+            for (int i = 1; i < words.Length; i++)
+            {
+                if (words[i].Length > words[indexMaxWord].Length)
+                {
+                    indexMaxWord = i;
+                }
+            }
+
+            return words[indexMaxWord];
+        }
+
+        public static string GetLongestWordsList(string message)
+        {
+            StringBuilder longestWords = new StringBuilder();
+            Regex regex = new Regex(@"\w{8,}", RegexOptions.IgnoreCase);
+
+            foreach (Match match in regex.Matches(message))
+            {
+                longestWords.AppendLine(match.ToString() + ";");
+            }
+
+            return longestWords.ToString();
+        }
     }
 }
