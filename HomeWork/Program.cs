720b6e2d3a4358ba33881e768e1e948cf8e1dabf
Maxim Pokrovsky <maxim.pokrovsky@outlook.com>
2022-04-02 20:37:16 +0300

    Написал программу для проверки введенного логина с использование регулярных выражений и без.

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using MP.Utils;
 
 
@@ -9,9 +10,10 @@ namespace HomeWork
     {
         static void Main(string[] args)
         {
-            ConsoleHelper.StartSettings("Начало программы");
-            ShowMenu();
-            Console.Clear();
+            //ConsoleHelper.StartSettings("Начало программы");
+            //ShowMenu();
+            //Console.Clear();
+            Task1("Проверка логина");
             Console.WriteLine("Программа завершена.");
             ConsoleHelper.Pause();
         }
@@ -20,7 +22,7 @@ namespace HomeWork
 
         static void ShowMenu()
         {
-            string nameTask1 = "";
+            string nameTask1 = "Проверка логина";
             string nameTask2 = "";
             string nameTask3 = "";
             string nameTask4 = "";
@@ -37,6 +39,7 @@ namespace HomeWork
                 switch (Console.ReadLine())
                 {
                     case "1":
+                        Task1(nameTask1);
                         break;
                     case "2":
                         break;
@@ -55,6 +58,91 @@ namespace HomeWork
 
                 ConsoleHelper.Pause();
             }
+        }
+
+        #endregion
+
+        #region Task 01
+
+        /*
+         
+                Создать программу, которая будет проверять корректность ввода логина. 
+                Корректным логином будет строка от 2 до 10 символов, 
+                содержащая только буквы латинского алфавита или цифры, при этом цифра не может быть первой:
+                    а) без использования регулярных выражений;
+                    б) **с использованием регулярных выражений.
+         
+         */
+
+        static void Task1(string taskName)
+        {
+            ConsoleHelper.StartSettings(taskName);
+            bool isCorrectLogin;
+
+            do
+            {
+                Console.Write("Введите логин: ");
+                isCorrectLogin = CheckLoginIsCorrectWithoutRegex(Console.ReadLine());
+
+                if (isCorrectLogin == false)
+                {
+                    Console.WriteLine("Корректный логин:\n 1) от 2 до 10 символов\n 2) только буквы латинского алфавита или цифры\n 3) цифра не может быть первой\n");
+                }
+                else
+                {
+                    Console.WriteLine("Логин введен корректно.");
+                }
+            }
+            while (isCorrectLogin == false);
+
+            Console.WriteLine("==================");
+            Console.WriteLine("Проверка логина с использованием регулярных выражений.");
+
+            do
+            {
+                Console.Write("Введите логин: ");
+                isCorrectLogin = CheckLoginIsCorrectWithRegex(Console.ReadLine());
+
+                if (isCorrectLogin == false)
+                {
+                    Console.WriteLine("Корректный логин:\n 1) от 2 до 10 символов\n 2) только буквы латинского алфавита или цифры\n 3) цифра не может быть первой\n");
+                }
+                else
+                {
+                    Console.WriteLine("Логин введен корректно.");
+                }
+            }
+            while (isCorrectLogin == false);
+
+        }
+
+        static bool CheckLoginIsCorrectWithoutRegex(string login)
+        {
+            if (login.Length < 2 || login.Length > 10)
+            {
+                return false;
+            }
+
+            if (char.IsDigit(login[0]))
+            {
+                return false;
+            }
+
+            foreach (char c in login)
+            {
+                if (char.IsLetterOrDigit(c) == false || c > 'z')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        static bool CheckLoginIsCorrectWithRegex(string login)
+        {
+            Regex regLogin = new Regex(@"^[A-Za-z]\w{1,9}$", RegexOptions.IgnoreCase);
+            return regLogin.IsMatch(login);
         }
 
         #endregion
