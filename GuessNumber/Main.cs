ccb8da8cfcce46bb044e6a8e8864240b9f89d4a7
Maxim Pokrovsky <maxim.pokrovsky@outlook.com>
2022-04-10 22:12:30 +0300

    Реализовал игру "Угадай число".

--- GuessNumber/Main.cs
+++ GuessNumber/Main.cs
@@ -25,9 +25,64 @@ namespace GuessNumber
 
     public partial class Main : Form
     {
+        private Random _random = new Random();
+        private int _hiddenNumber;
+        private int _enteredNumber;
+
         public Main()
         {
             InitializeComponent();
+            Reset();
+        }
+
+        private void btn_EnterNumber_Click(object sender, EventArgs e)
+        {
+            new NumberEntry(SetNumber).Show();
+            btn_EnterNumber.Enabled = false;
+        }
+
+        private void Reset()
+        {
+            _hiddenNumber = _random.Next(1, 101);
+            lbl_info.Text = "Для ввода\nнажми кнопку Ввести число";
+            btn_EnterNumber.Enabled = true;
+        }
+
+        private void SetNumber(int number)
+        {
+            _enteredNumber = number;
+            btn_EnterNumber.Enabled = true;
+            CheckWin();
+
+        }
+
+        private void CheckWin()
+        {
+            if (_enteredNumber > _hiddenNumber)
+            {
+                lbl_info.Text = "Загаданное число меньше твоего!";
+            }
+            else if (_enteredNumber < _hiddenNumber)
+            {
+                lbl_info.Text = "Загаданное число больше твоего!";
+            }
+            else
+            {
+                if (MessageBox.Show($"ДА! Загаданное число {_hiddenNumber}! Повторить?", "Победа",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+                {
+                    Reset();
+                }
+                else
+                {
+                    Close();
+                }
+            }
+        }
+
+        private void btn_restart_Click(object sender, EventArgs e)
+        {
+            Reset();
         }
     }
 }
---
+++ GuessNumber/NumberEntry.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace GuessNumber
+{
+    partial class NumberEntry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tb_userEnter = new System.Windows.Forms.TextBox();
+            this.lbl_info = new System.Windows.Forms.Label();
+            this.btn_OK = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tb_userEnter);
+            this.panel1.Controls.Add(this.lbl_info);
+            this.panel1.Location = new System.Drawing.Point(0, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(383, 52);
+            this.panel1.TabIndex = 0;
+            // 
+            // tb_userEnter
+            // 
+            this.tb_userEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb_userEnter.Location = new System.Drawing.Point(211, 8);
+            this.tb_userEnter.Name = "tb_userEnter";
+            this.tb_userEnter.Size = new System.Drawing.Size(154, 34);
+            this.tb_userEnter.TabIndex = 1;
+            // 
+            // lbl_info
+            // 
+            this.lbl_info.AutoSize = true;
+            this.lbl_info.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl_info.Location = new System.Drawing.Point(3, 13);
+            this.lbl_info.Name = "lbl_info";
+            this.lbl_info.Size = new System.Drawing.Size(202, 29);
+            this.lbl_info.TabIndex = 0;
+            this.lbl_info.Text = "Введите число:";
+            // 
+            // btn_OK
+            // 
+            this.btn_OK.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_OK.Location = new System.Drawing.Point(267, 93);
+            this.btn_OK.Name = "btn_OK";
+            this.btn_OK.Size = new System.Drawing.Size(103, 48);
+            this.btn_OK.TabIndex = 1;
+            this.btn_OK.Text = "OK";
+            this.btn_OK.UseVisualStyleBackColor = true;
+            this.btn_OK.Click += new System.EventHandler(this.btn_OK_Click);
+            // 
+            // NumberEntry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 153);
+            this.Controls.Add(this.btn_OK);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(400, 200);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(400, 200);
+            this.Name = "NumberEntry";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ввод числа";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox tb_userEnter;
+        private System.Windows.Forms.Label lbl_info;
+        private System.Windows.Forms.Button btn_OK;
+    }
+}
