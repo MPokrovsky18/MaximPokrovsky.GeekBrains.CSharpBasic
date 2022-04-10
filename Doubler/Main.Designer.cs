956d082022d4a5a3cb4a1d21ed4195dd27473e8a
Maxim Pokrovsky <maxim.pokrovsky@outlook.com>
2022-04-10 16:35:28 +0300

    Реализовал необходимый интерфейс для приложения.

--- Doubler/Main.Designer.cs
+++ Doubler/Main.Designer.cs
@@ -28,19 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblComputerNumber = new System.Windows.Forms.Label();
+            this.lblUserNumber = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnPlus = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblComputerNumber
+            // 
+            this.lblComputerNumber.AutoSize = true;
+            this.lblComputerNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblComputerNumber.Location = new System.Drawing.Point(12, 69);
+            this.lblComputerNumber.Name = "lblComputerNumber";
+            this.lblComputerNumber.Size = new System.Drawing.Size(221, 29);
+            this.lblComputerNumber.TabIndex = 3;
+            this.lblComputerNumber.Text = "Получите число: ";
+            // 
+            // lblUserNumber
+            // 
+            this.lblUserNumber.AutoSize = true;
+            this.lblUserNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblUserNumber.Location = new System.Drawing.Point(12, 161);
+            this.lblUserNumber.Name = "lblUserNumber";
+            this.lblUserNumber.Size = new System.Drawing.Size(213, 29);
+            this.lblUserNumber.TabIndex = 4;
+            this.lblUserNumber.Text = "Текущее число: ";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnReset.Location = new System.Drawing.Point(281, 12);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(139, 86);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "Новая игра";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnMultiply.Location = new System.Drawing.Point(281, 104);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(139, 86);
+            this.btnMultiply.TabIndex = 6;
+            this.btnMultiply.Text = "x2";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnPlus.Location = new System.Drawing.Point(281, 196);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(139, 86);
+            this.btnPlus.TabIndex = 7;
+            this.btnPlus.Text = "+1";
+            this.btnPlus.UseVisualStyleBackColor = true;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(579, 443);
+            this.ClientSize = new System.Drawing.Size(432, 303);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblUserNumber);
+            this.Controls.Add(this.lblComputerNumber);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(450, 350);
             this.Name = "Main";
-            this.Text = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Удвоитель";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Label lblComputerNumber;
+        private System.Windows.Forms.Label lblUserNumber;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnPlus;
     }
 }
